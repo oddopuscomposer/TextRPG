23fc6348c0fc133aa20c06bf24dc824a2578a1c4
cchaffin <cchaffin@marketamerica.com>
2018-07-02 09:51:03 -0400

    edited save template, fixed template edit bug

--- game.py
+++ game.py
@@ -1,5 +1,4 @@
-import json
-from game_utilities import process_json
+from game_utilities import process_json, write_json
 
 
 def launch(setting):
@@ -8,22 +7,22 @@ def launch(setting):
     :param type:
     :return:
     """
-    player_data = process_json("saves.json")["saves"]
+    player_data = process_json("saves.json")
     class_data = process_json("classes.json")
 
-    saves = []
-    for key in player_data.keys():
-        saves.append(key)
+    saves = player_data["xref"]
 
     if setting == "saves":
         print(saves)
         save = input("Choose a save data: ")
-        character = player_data[save]
+        character = player_data["saves"][save]
     if setting == "new":
-        character = player_data["save_template"]
+        character = player_data["saves"]["start_template"].copy()
         choose_status = False
         while not choose_status:
             name = input("What is your name?: ")
+            player_data["saves"][name] = character
+            character = player_data["saves"][name]
             character["name"] = name
             print(class_data["xref"])
             cls = input("Choose your class: ")
@@ -34,7 +33,9 @@ def launch(setting):
                 choose_status = True
             else:
                 print("Class doesnt exist")
-                choose_status = False
+        player_data["xref"].append(character["name"])
+        write_json("saves.json", player_data)
+
     start_game(character)
     print(character)
 
@@ -47,7 +48,46 @@ def start_game(character):
     """
     game_status = True
     print("Welcome to the game!")
+    print(character)
     while game_status:
         print("goto, shop, stats, rest")
         selection = input("Select an option: ")
+        if selection == "stats":
+            show_stats(character)
+
+
+def show_stats(character):
+    """
+    Shows character stats/current save
+    :return:
+    """
+    print("name: " + character["name"])
+    print("level: " + character["level"])
+    print("class: " + character["class"])
+    print("health: " + character["health"])
+    print("mana: " + character["mana"])
+    print("att: " + character["stats"]["att"])
+    print("def: " + character["stats"]["def"])
+    print("evd: " + character["stats"]["evd"])
+    print("exp: " + character["stats"]["exp"])
+    print("next level: " + character["stats"]["exp_req"])
+    print("training points: " + character["training_points"])
+    print("skills: " + character["skills"])
+    print("location: " + character["location"])
+
+
+
+
+
+
+def rest():
+    pass
+
+
+def go_to():
+    pass
+
+
+def shop():
+    pass
 
